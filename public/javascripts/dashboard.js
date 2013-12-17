0b3087a3c3323c57f568f9d0d200c3750e7a2aa1
Riccardo Volonterio <voloxxx@gmail.com>
2013-12-17 17:46:35 +0100

    Added line charts.

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -9,8 +9,8 @@ var drawPieChart = function( label, sample, status ) {
     data.push( [ status[ i ], sample[ i ] ] );
   }
 
-  data = _.sortBy( data, function( d ) {
-    return d[ 0 ];
+  data = data.sort( function( a, b ) {
+    return a[ 0 ] - b[ 0 ];
   } );
 
 
@@ -28,9 +28,9 @@ var drawPieChart = function( label, sample, status ) {
         spacing: [ 0, 0, 0, 0 ]
       },
       colors: [
-        '#d9534f',
-        '#f0ad4e',
-        '#5bc0de'
+        '#cccccc',
+        '#5cb85c',
+        '#d9534f'
       ],
       title: {
         text: label,
@@ -67,6 +67,46 @@ var drawPieChart = function( label, sample, status ) {
 
 };
 
+
+
+function drawLineChart( activeExecutions, activeObjects ) {
+  $( '#other' ).highcharts( {
+    chart: {
+      zoomType: 'x',
+      type: 'spline'
+    },
+    title: {
+      text: 'Executions'
+    },
+    xAxis: {
+      type: 'datetime'
+    },
+    yAxis: [ {
+      title: {
+        text: 'Active executions (#)'
+      },
+      tickInterval: 1,
+      min: 0
+    }, {
+      title: {
+        text: 'Active objects (#)'
+      },
+      tickInterval: 1,
+      opposite: true,
+      min: 0
+    } ],
+    series: [ {
+      name: 'Active executions',
+      data: activeExecutions,
+      yaxis: 0
+    }, {
+      name: 'Active objects',
+      data: activeObjects,
+      yaxis: 1
+    } ]
+  } );
+}
+
 $.getJSON( baseUrl + 'api/task/' + taskId + '/stats?raw=true' )
   .done( function( stats ) {
 
@@ -78,4 +118,48 @@ $.getJSON( baseUrl + 'api/task/' + taskId + '/stats?raw=true' )
 
     drawPieChart( 'Executions', [ createdExecutions, closedExecutions, invalidExecutions ], executionStatuses );
 
+
+    var execList = stats.raw;
+    var activeExecutions = [];
+
+    function toUTC( dateString ) {
+      var date = new Date( dateString );
+      return Date.UTC( date.getUTCFullYear(), date.getUTCMonth() - 1, date.getUTCDate(), date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds(), date.getUTCMilliseconds() );
+      //return date;
+    }
+
+    $.each( execList, function() {
+      var exec = this;
+      activeExecutions.push( {
+        date: toUTC( exec.createdDate ),
+        value: 1
+      } );
+
+      if ( exec.status !== 'CREATED' ) {
+        activeExecutions.push( {
+          date: toUTC( exec.closedDate || exec.invalidDate ),
+          value: -1
+        } );
+      }
+
+    } );
+
+    activeExecutions.sort( function( a, b ) {
+      return a.date - b.date;
+    } );
+
+    var val = 0;
+    activeExecutions = $.map( activeExecutions, function( exec ) {
+      val += exec.value;
+      return {
+        x: exec.date,
+        y: val,
+        marker: {
+          enabled: false
+        }
+      };
+    } );
+
+    var activeObjects = [];
+    drawLineChart( activeExecutions, activeObjects );
   } );
