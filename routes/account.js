d3a4b76f4c9103391b425cfc5c633f01683aa347
Volox <voloxxx@gmail.com>
2014-04-02 14:19:24 +0200

    Fixed account page

--- routes/account.js
+++ routes/account.js
@@ -6,6 +6,7 @@ var log = CS.log.child( { component: 'Account Routes' } );
 exports.index = function( req, res ) {
   log.trace( 'Render account view' );
   res.render( 'account', {
-    title: 'Account'
+    title: 'Account',
+    socialMap: CS.social
   } );
 };
